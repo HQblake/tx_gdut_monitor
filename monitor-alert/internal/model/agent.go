23569a73ccf1d06108bb5f5500fa0615af3f950f
HQBlake <huangq.work@outlook.com>
2022-01-29 15:48:55 +0800

    为AgentReport添加Dimensions字段

--- monitor-alert/internal/model/agent.go
+++ monitor-alert/internal/model/agent.go
@@ -2,9 +2,10 @@ package model
 
 // AgentReport Agent采集信息上报的数据结构
 type AgentReport struct {
-	IP        string             // Agent的IP地址
-	port      string             // Agent的端口号
-	Local     string             // Agent的区域
-	Timestamp int                // 信息上报的时间戳
-	Metrics   map[string]float64 // 指标类型: 指标值
+	IP         string             // Agent的IP地址
+	port       string             // Agent的端口号
+	Local      string             // Agent的区域
+	Timestamp  int64              // 信息上报的时间戳
+	Metrics    map[string]float64 // 指标类型: 指标值
+	Dimensions map[string]string  // Agent维度
 }
