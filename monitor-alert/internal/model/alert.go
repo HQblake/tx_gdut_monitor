4c2046a93ee7b8dfe2e9c967fa4355f9429ed530
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-03 21:25:25 +0800

    告警信息结构补充json tag

--- monitor-alert/internal/model/alert.go
+++ monitor-alert/internal/model/alert.go
@@ -4,19 +4,19 @@ import "sync"
 
 // AlertInfo 记录某台Agent的所有异常指标信息
 type AlertInfo struct {
-	AgentID string                // agent 标识：agent.Local-agent.IP
-	Metrics map[string]MetricInfo // 指标告警信息
+	AgentID string                `json:"agent_id"` // agent 标识
+	Metrics map[string]MetricInfo `json:"metrics"`  // 指标告警信息
 }
 
 // MetricInfo 记录异常指标信息
 type MetricInfo struct {
-	Metric    string  // 指标类型
-	Value     float64 // 指标值
-	Threshold float64 // 阈值
-	Method    int8    // 聚合方式
-	Level     int8    // 告警等级
-	Duration  string  // 持续时间
-	Start     string  // 开始时间
+	Metric    string  `json:"metric"`    // 指标类型
+	Value     float64 `json:"value"`     // 指标值
+	Threshold float64 `json:"threshold"` // 阈值
+	Method    int8    `json:"method"`    // 聚合方式
+	Level     int8    `json:"level"`     // 告警等级
+	Duration  string  `json:"duration"`  // 持续时间
+	Start     string  `json:"start"`     // 开始时间
 }
 
 var AlertInfoPool = &sync.Pool{
