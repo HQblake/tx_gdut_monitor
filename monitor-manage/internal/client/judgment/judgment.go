f4d2f3d8a0fd8e13279a74816651bbe32dc8177c
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-08 22:21:36 +0800

    pb协议更新整合

--- monitor-manage/internal/client/judgment/judgment.go
+++ monitor-manage/internal/client/judgment/judgment.go
@@ -1,8 +1,20 @@
 package judgment
 
-import "gitee.com/zekeGitee_admin/tx_gdut_monitor/monitor-manage/internal/model"
+import (
+	proto "gitee.com/zekeGitee_admin/tx_gdut_monitor/monitor-manage/internal/client/judgment/gen"
+	"gitee.com/zekeGitee_admin/tx_gdut_monitor/monitor-manage/internal/model"
+	"google.golang.org/grpc"
+	"sync"
+)
 
-// IJudgment 服务接口化，对外提供Update方法，内部调用Grpc
+// IJudgment 服务接口化，对外提供Update方法，内部实际调用Grpc，尽可能解耦
 type IJudgment interface {
 	Update(rule *model.AgentRule) error
 }
+type Service struct {
+	client *proto.RuleUpdaterClient
+	conn   *grpc.ClientConn
+	once   sync.Once
+}
+
+
