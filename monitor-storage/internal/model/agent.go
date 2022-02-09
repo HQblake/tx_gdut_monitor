f70202246924194e42e7acf3d92fba8a3e3a3227
HQBlake <huangq.work@outlook.com>
2022-02-09 13:36:18 +0800

    manage2store与judgment2store协议添加double Threshold 字段

--- monitor-storage/internal/model/agent.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type AgentInfo struct {
-	Ip     string   `json:"ip"`
-	Port   string   `json:"port"`
-	Local  string   `json:"local"`
-	IsLive bool     `json:"is_live"`
-	Metric []string `json:"metric"`
-}
