586be211d99ce3329f3bb2fdc20cb9b570e0dc59
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-10 18:32:36 +0800

    管理结构临时提交，更新对接协议

--- monitor-manage/internal/service/http/agent.go
+++
@@ -1,8 +0,0 @@
-package http
-
-import "gitee.com/zekeGitee_admin/tx_gdut_monitor/monitor-manage/internal/model"
-
-// IAgent 页面管理Agent的相关接口
-type IAgent interface {
-	GetAgents() []*model.AgentInfo
-}
--- monitor-manage/internal/service/http/config.go
+++
@@ -1,9 +0,0 @@
-package http
-
-// IConfig 关于配置管理的相关操作
-type IConfig interface {
-	// 发送服务管理
-
-	// 判定服务管理
-
-}
