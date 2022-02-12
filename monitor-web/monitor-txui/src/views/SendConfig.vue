00599733bc39c12b7927691154ac6e5948b210f4
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-13 00:26:12 +0800

    docker file

---
+++ monitor-web/monitor-txui/src/views/SendConfig.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class='headline'>
+      <h3 > 发送配置 </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'sendConfig',
+  components: {
+  },
+  data () {
+    return {
+      test: 'test'
+    }
+  },
+  created () {
+
+  },
+  methods: {
+  }
+}
+</script>
+<style lang='less'>
+</style>
---
+++ monitor-web/monitor-txui/src/views/SendDetail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class='headline'>
+      <h3 > {{ip}} - {{local}} 发送配置详情 </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'sendDetail',
+  components: {
+  },
+  data () {
+    return {
+      test: 'test',
+      ip: '',
+      local: ''
+    }
+  },
+  created () {
+    this.ip = this.$route.params.ip
+    this.local = this.$route.params.local
+  },
+  methods: {
+  }
+}
+</script>
+<style lang='less'>
+</style>
