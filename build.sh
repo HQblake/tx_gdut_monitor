09955bea27c9fd93fe6221a7cb4a60d3ac15441b
HQBlake <huangq.work@outlook.com>
2022-02-15 17:11:05 +0800

    调整storage与alert的Dockerfile

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 # 运行 monitor-storage
-docker build -t monitor-storage:1.0 -f ./build/monitor-storage/Dockerfile --rm .
-docker run -d -p 8080:8080 -v /mnt/f/Docker/monitor/data:/data --name storage monitor-storage:1.0
+docker build -t monitor-storage:1.0 --force-rm=true ./build/monitor-storage
+docker run -d -p 8080:8080 -v /mnt/f/Docker/monitor/data/storage:/data --name storage monitor-storage:1.0
 
-docker build -t monitor-alert:1.0 -f ./build/monitor-alert/Dockerfile --rm .
-docker run -d -p 8081:8081 -v /mnt/f/Docker/monitor/data:/data --name alert monitor-alert:1.0
+# 运行 monitor-alert
+docker build -t monitor-alert:1.0 --force-rm=true ./build/monitor-alert
+docker run -d -p 8081:8081 -v /mnt/f/Docker/monitor/data/alert:/data --name alert monitor-alert:1.0
 
-docker build -t monitor-manage:1.0 -f ./build/monitor-manage/Dockerfile --rm .
-docker build -t monitor-agent:1.0 -f ./build/monitor-agent/Dockerfile --rm .
+# 运行 monitor-manage
+docker build -t monitor-manage:1.0 --force-rm=true ./build/monitor-manage
+docker run -d -p 8082:8082 -p 8083:8083 -v /mnt/f/Docker/monitor/data/manage:/data --name manage monitor-manage:1.0
+
+# 运行 monitor-agent
+docker build -t monitor-agent:1.0 --force-rm=true ./build/monitor-agent
+docker run -d -p 8084:8084 -v /mnt/f/Docker/monitor/data/agent:/data --name agent monitor-agent:1.0
