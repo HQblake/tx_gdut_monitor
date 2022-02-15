c1ecd9ac6f6b59068a1256f8997d4f484562c139
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-15 22:30:30 +0800

    卷积完善

--- build_linux.sh
+++ build_linux.sh
@@ -3,19 +3,19 @@ docker network create --driver bridge --subnet 192.168.0.0/16 --gateway 192.168.
 
 # 运行 monitor-storage
 docker build -t monitor-storage:1.0 --force-rm=true ./build/monitor-storage
-docker run -d -p 8080:8080 -v /home/monitor/data/storage:/data --name storage --net monitor monitor-storage:1.0
+docker run -d -p 8080:8080 -v /home/monitor/data/storage:/data/storage --name storage --net monitor monitor-storage:1.0
 
 # 运行 monitor-alert
 docker build -t monitor-alert:1.0 --force-rm=true ./build/monitor-alert
-docker run -d -p 8081:8081 -v /home/f/Docker/monitor/data/alert:/data --name alert --net monitor monitor-alert:1.0
+docker run -d -p 8081:8081 -v /home/monitor/data/alert:/data/alert --name alert --net monitor monitor-alert:1.0
 
 # 运行 monitor-manage
 docker build -t monitor-manage:1.0 --force-rm=true ./build/monitor-manage
-docker run -d -p 8082:8082 -p 8088:8083 -v /home/monitor/data/manage:/data --name manage --net monitor monitor-manage:1.0
+docker run -d -p 8082:8082 -p 8083:8083 -v /home/monitor/data/manage:/data/monitor --name manage --net monitor monitor-manage:1.0
 
 # 运行 monitor-agent
 docker build -t monitor-agent:1.0 --force-rm=true ./build/monitor-agent
-docker run -d -p 8084:8084 -v /home/monitor/data/agent:/data --name agent --net monitor monitor-agent:1.0
+docker run -d -p 8084:8084 -v /home/monitor/data/agent:/data/agent --name agent --net monitor monitor-agent:1.0
 
 # 运行ui模块
 docker build -t monitor-web:1.0 --force-rm=true ./build/monitor-web
