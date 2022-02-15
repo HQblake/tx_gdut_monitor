388d7c4431f30a3b93362739f9d492d63cb650af
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-15 23:39:24 +0800

    update url

--- monitor-web/monitor-txui/dist/service-worker.js
+++ monitor-web/monitor-txui/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03355eac5051cc389111f1fc62656e6d.js"
+  "/precache-manifest.99e059da6dee02fd922dd7fe1a573b1c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "monitor-txui"});
