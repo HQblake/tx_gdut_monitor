b31183dcedc2992a25fa6d17c188195436eb61c2
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-15 14:24:41 +0800

    页面构建版本

---
+++ monitor-web/monitor-txui/dist/precache-manifest.a711eb2c71610c1ad88f4e0e84240602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "74957321c3735f4ba909",
+    "url": "/js/chunk-vendors.aa0cd752.js"
+  },
+  {
+    "revision": "ab8f3f177d82db5d52b9",
+    "url": "/js/app.8a17f10e.js"
+  },
+  {
+    "revision": "42726eade55b006d38c1bcbc0acfe7aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "ab8f3f177d82db5d52b9",
+    "url": "/css/app.ab0746a4.css"
+  }
+];
