8e9fb42ca68a7a45072068ab1a2e14307ffc2c0b
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-02-16 08:38:19 +0800

    cors

--- monitor-manage/cmd/main.go
+++ monitor-manage/cmd/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"flag"
-	"log"
-	"net"
-	"net/http"
 	"gitee.com/zekeGitee_admin/tx_gdut_monitor/monitor-manage/configs"
 	"gitee.com/zekeGitee_admin/tx_gdut_monitor/monitor-manage/internal/service"
 	"gitee.com/zekeGitee_admin/tx_gdut_monitor/monitor-manage/internal/service/http"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"net"
 )
 
 var configPath string
@@ -104,7 +103,7 @@ func Cors() gin.HandlerFunc {
 	    c.Header("Access-Control-Allow-Credentials", "true")
 		
 		if method == "OPTIONS" {
-			c.JSON(http.StatusOK, "ok!")
+			c.JSON(200, "ok!")
 		}
 
 		defer func() {
