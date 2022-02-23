0bcee8c34ffb56ca1356062c36cf478324205e7c
HQBlake <huangq.work@outlook.com>
2022-02-23 18:05:48 +0800

    修改data文件夹配置

--- build_linux.sh
+++ build_linux.sh
@@ -13,10 +13,17 @@ docker run -d -p 8081:8081 -v /home/monitor/data/alert:/data/alert --name alert 
 docker build -t monitor-manage:1.0 --force-rm=true ./build/monitor-manage
 docker run -d -p 8082:8082 -p 8083:8083 -v /home/monitor/data/manage:/data/manage --name manage --net monitor monitor-manage:1.0
 
-# 运行 monitor-agent
+# 运行 5台 monitor-agent
 docker build -t monitor-agent:1.0 --force-rm=true ./build/monitor-agent
-docker run -d -p 8084:8084 -v /home/monitor/data/agent:/data/agent --name agent --net monitor monitor-agent:1.0
+docker run -d -p 8085:8084 -v /home/monitor/data/agent/agent-01:/data/agent --name agent-01 --net monitor monitor-agent:1.0
+docker run -d -p 8086:8084 -v /home/monitor/data/agent/agent-02:/data/agent --name agent-02 --net monitor monitor-agent:1.0
+docker run -d -p 8087:8084 -v /home/monitor/data/agent/agent-03:/data/agent --name agent-03 --net monitor monitor-agent:1.0
+docker run -d -p 8088:8084 -v /home/monitor/data/agent/agent-04:/data/agent --name agent-04 --net monitor monitor-agent:1.0
+docker run -d -p 8089:8084 -v /home/monitor/data/agent/agent-05:/data/agent --name agent-05 --net monitor monitor-agent:1.0
 
 # 运行ui模块
 docker build -t monitor-web:1.0 --force-rm=true ./build/monitor-web
-docker run -d -p 8085:80 -v /home/monitor/data/web:/usr/share/nginx/html/static -v /home/monitor/data/web/nginx:/etc/nginx/conf.d --name web --net monitor monitor-web:1.0
+docker run -d -p 8084:80 -v /home/monitor/data/web:/usr/share/nginx/html/static -v /home/monitor/data/web/nginx:/etc/nginx/conf.d --name web --net monitor monitor-web:1.0
+
+# 删除中间镜像
+docker rmi -f $(docker images | grep "none" | awk '{print $3}')
